f9b8dcb661d280ffe8d9b3de505ca3098936436f
Maksim Shepelevich <maximshep2001@gmail.com>
2021-03-31 23:16:31 +0300

    first commit

---
+++ outputConsole/src/main/java/by/issoft/firstMod/Util.java
@@ -0,0 +1,35 @@
+package by.issoft.firstMod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Util {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    static int binarySearch(int searchNumber, int[] array, int start, int end){
+
+        int middle = (start + end)/2;
+
+        if(end < start){
+            return -1;
+        }
+
+        try {
+            if (searchNumber < array[middle]) {
+                return binarySearch(searchNumber, array, start, middle - 1);
+            }
+
+            if (searchNumber > array[middle]) {
+                return binarySearch(searchNumber, array, middle + 1, end);
+            }
+
+            if (searchNumber == array[middle]) {
+                return middle;
+            }
+        } catch (IllegalArgumentException e){
+            logger.error("Incorrect number.");
+        }
+
+        return -1;
+    }
+
+}
---
+++ outputFile/src/main/java/by/issoft/secondMod/Main.java
@@ -0,0 +1,28 @@
+package by.issoft.secondMod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.debug("The program starts.");
+
+        logger.debug("Creating an array.");
+        int[] array = { 2, 3, 4, 10, 40, 50 };
+        logger.debug("Array is created.");
+        int amountElements = array.length;
+        int searchNumber = 10;
+
+        logger.debug("Start recursive binary search.");
+        int result = Util.binarySearch(searchNumber, array, 0, amountElements-1);
+        logger.debug("Recursive binary search is over.");
+        if (result == -1)
+            logger.debug("Element not present.");
+        else
+            logger.debug("Result: element found at index " + result+".");
+        logger.debug("The end of the program.");
+
+    }
+
+}
