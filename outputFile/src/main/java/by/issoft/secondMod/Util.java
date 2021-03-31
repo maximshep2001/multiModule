9915775d95a0c192bfff32cba27939b500a9dbbf
Maksim Shepelevich <maximshep2001@gmail.com>
2021-04-01 00:25:44 +0300

    fourth commit

--- outputFile/src/main/java/by/issoft/secondMod/Util.java
+++ outputFile/src/main/java/by/issoft/secondMod/Util.java
@@ -1,35 +1,35 @@
 package by.issoft.secondMod;
 
-import by.issoft.firstMod.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Util {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    public static int binarySearch(int searchNumber, int[] array, int start, int end){
 
-        int middle = (start + end)/2;
+    private static final Logger logger = LoggerFactory.getLogger(Util.class);
 
-        if(end < start){
+    public static int binarySearch(int searchNumber, int[] array, int start, int end) {
+        logger.debug("Find the middle element.");
+        int middle = (start + end) / 2;
+        logger.debug("Checking an array.");
+        if (end < start) {
+            logger.error("Array boundaries are specified incorrectly.");
             return -1;
         }
-
-        try {
-            if (searchNumber < array[middle]) {
+        logger.debug("Checking whether the element on the left side of the array is from the middle or not.");
+        if (searchNumber < array[middle]) {
+            logger.debug("The element on the left side of the array from the middle.");
                 return binarySearch(searchNumber, array, start, middle - 1);
             }
-
-            if (searchNumber > array[middle]) {
-                return binarySearch(searchNumber, array, middle + 1, end);
-            }
-
-            if (searchNumber == array[middle]) {
-                return middle;
-            }
-        } catch (IllegalArgumentException e){
-            logger.error("Incorrect number.");
+        logger.debug("Checking whether the element on the right side of the array is from the middle or not.");
+        if (searchNumber > array[middle]) {
+            logger.debug("The element on the right side of the array from the middle.");
+            return binarySearch(searchNumber, array, middle + 1, end);
         }
-
+        logger.debug("Checking if a search element is equal to a found element.");
+        if (searchNumber == array[middle]) {
+            logger.debug("The search element is equal to a found element.");
+            return middle;
+        }
         return -1;
     }
 
